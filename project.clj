7de3f8967b8599b91cf757b45730bdc1a47eba1e
Janne Himanka <janne.himanka@iki.fi>
2019-01-18 14:22:49 +0200

    Version bumps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [fipp "0.6.12"]
-                 [aero "1.1.2"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [clj-http "3.7.0"]]
+                 [fipp "0.6.14"]
+                 [aero "1.1.3"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [clj-http "3.9.1"]]
   :main saa.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
